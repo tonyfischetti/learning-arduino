1518ff5de4df6713dbe76e9e742bb276f2d20a3b
Tony Fischetti <tony.fischetti@gmail.com>
2023-01-28 07:44:31 -0500

    can't test. on plane. probably works. but you never know

--- rgb-led-color-shift/rgb-led-color-shift_2-with-dim-and-strobe.ino
+++ rgb-led-color-shift/rgb-led-color-shift_2-with-dim-and-strobe.ino
@@ -14,34 +14,60 @@
 
 #define DEBUG false
 
+// pins that contol each color of the RGB LED(s)
 #define RED_LED   11
 #define GREEN_LED 10
 #define BLUE_LED  9
 
+// value to change the color by on each step
 #define STEP_DELTA 5
 
+// number of milliseconds to wait for buttons, etc. to settle
 #define EPSILON 250
 
 #define PRIMARY_SENSOR_IN    A0
 #define PRIMARY_SENSOR_OUT   2
 #define SECONDARY_SENSOR_IN  A1
 #define SECONDARY_SENSOR_OUT 4
+// TODO: tertiary sensors
 
+// The indices into the `current_rgb` array for each color
 #define RED_INDEX   0
 #define GREEN_INDEX 1
 #define BLUE_INDEX  2
 
 
-byte step_delay = 15;
-byte strobe_delay = 0;
-
+// holds the values for each RGB channel (globally)
 byte current_rgb[3] = {255, 255, 255};
 
+// holds the index (into pattern_functions) for the current
+// pattern's function pointer
+byte current_pattern_fun_index = 0;
+
+// counter of steps. will overflow. that's ok
 byte counter = 0;
+
 byte check_sensors_about_every = 10;
 
 
+
+
+void nothing_function () {
+    // correct?
+    return NULL;
+}
+
+typedef void (*SensorUpdateFunction) ();
+SensorUpdateFunction update_primary_sensor  = nothing_function;
+SensorUpdateFunction update_secondary_sensor = nothing_function;
+
+typedef void (*PatternFunction) ();
+PatternFunction current_pattern_function = nothing_function;
+
+
+
 #if DEBUG
+// TODO: more debug info (pattern, etc...)
 void debug_values() {
     char buff[20];
     sprintf(buff, "(%d, %d, %d)",
@@ -58,8 +84,8 @@ void write_RGB_colors() {
     analogWrite(GREEN_LED, current_rgb[GREEN_INDEX]);
     analogWrite(BLUE_LED,  current_rgb[BLUE_INDEX]);
     if (counter % check_sensors_about_every == 0) {
-        update_step_delay();
-        update_strobe_delay();
+        update_primary_sensor();
+        update_secondary_sensor();
     }
     #if DEBUG
     debug_values();
@@ -72,30 +98,41 @@ void write_RGB_zeroes() {
     analogWrite(BLUE_LED,  0);
 }
 
+
+
+/* --------------------------------------------------------- */
+
+/* PATTERN 0:
+ *
+ *   Shifts through all the colors. Speed, strobe, and brightness are
+ *   parameterized.
+ *
+ */
+
+byte step_delay_0 = 15;
+byte strobe_delay_0 = 0;
+
 void update_step_delay() {
-    step_delay = map(analogRead(PRIMARY_SENSOR_IN), 0, 1023, 1, 200);
+    step_delay_0 = map(analogRead(PRIMARY_SENSOR_IN), 0, 1023, 1, 200);
 }
 
 void update_strobe_delay() {
-    strobe_delay = map(analogRead(SECONDARY_SENSOR_IN), 0, 1023, 0, 255);
+    strobe_delay_0 = map(analogRead(SECONDARY_SENSOR_IN), 0, 1023, 0, 255);
 }
 
 void do_strobe_delay() {
-    if (strobe_delay > 27) {
+    if (strobe_delay_0 > 27) {
         // TODO: fix
         write_RGB_zeroes();
-        delay(strobe_delay);
+        delay(strobe_delay_0);
     }
 }
-
-
-/* ----------------------------------- */
 
 void bring_down_color(byte color_index) {
     while (current_rgb[color_index] >= STEP_DELTA) {
         current_rgb[color_index] = current_rgb[color_index] - STEP_DELTA;
         write_RGB_colors();
-        delay(step_delay);
+        delay(step_delay_0);
         do_strobe_delay();
     }
 }
@@ -104,12 +141,82 @@ void bring_up_color(byte color_index) {
     while (current_rgb[color_index] <= (255 - STEP_DELTA)) {
         current_rgb[color_index] = current_rgb[color_index] + STEP_DELTA;
         write_RGB_colors();
-        delay(step_delay);
+        delay(step_delay_0);
         do_strobe_delay();
     }
 }
 
-/* ----------------------------------- */
+void all_color_change_pattern() {
+
+    update_primary_sensor = update_step_delay;
+    update_secondary_sensor = update_strobe_delay;
+
+    // first sub-pattern
+    bring_down_color(RED_INDEX);
+    bring_down_color(GREEN_INDEX);
+    bring_up_color(RED_INDEX);
+    bring_down_color(BLUE_INDEX);
+    bring_up_color(GREEN_INDEX);
+    bring_up_color(BLUE_INDEX);
+
+    // second sub-pattern
+    bring_down_color(GREEN_INDEX);
+    bring_down_color(BLUE_INDEX);
+    bring_up_color(GREEN_INDEX);
+    bring_down_color(RED_INDEX);
+    bring_up_color(BLUE_INDEX);
+    bring_up_color(RED_INDEX);
+
+    // third sub-pattern
+    bring_down_color(BLUE_INDEX);
+    bring_down_color(RED_INDEX);
+    bring_up_color(BLUE_INDEX);
+    bring_down_color(GREEN_INDEX);
+    bring_up_color(RED_INDEX);
+    bring_up_color(GREEN_INDEX);
+
+    // TODO: goto end??
+}
+
+/* --------------------------------------------------------- */
+
+/* PATTERN 1:
+ *
+ *   Solid color with option to change the values for each RGB channel
+ *
+ */
+
+void update_red_brightness() {
+    current_rgb[RED_INDEX] = map(analogRead(PRIMARY_SENSOR_IN),
+                                            0, 1023, 0, 255);
+}
+
+void update_green_brightness() {
+    current_rgb[GREEN_INDEX] = map(analogRead(SECONDARY_SENSOR_IN),
+                                              0, 1023, 0, 255);
+}
+
+
+void solid_color_pattern() {
+
+    update_primary_sensor = update_red_brightness;
+    update_secondary_sensor = update_green_brightness;
+
+    current_rgb[0] = 255;
+    current_rgb[1] = 255;
+    current_rgb[2] = 255;
+    write_RGB_colors();
+
+    delay(100);
+}
+
+
+/* --------------------------------------------------------- */
+
+PatternFunction pattern_functions[2] = {
+    all_color_change_pattern,
+    solid_color_pattern
+};
 
 void setup() {
     pinMode(RED_LED, OUTPUT);
@@ -125,31 +232,10 @@ void setup() {
     #endif
 }
 
+
 void loop() {
 
-    // first pattern
-    bring_down_color(RED_INDEX);
-    bring_down_color(GREEN_INDEX);
-    bring_up_color(RED_INDEX);
-    bring_down_color(BLUE_INDEX);
-    bring_up_color(GREEN_INDEX);
-    bring_up_color(BLUE_INDEX);
-
-    // second pattern
-    bring_down_color(GREEN_INDEX);
-    bring_down_color(BLUE_INDEX);
-    bring_up_color(GREEN_INDEX);
-    bring_down_color(RED_INDEX);
-    bring_up_color(BLUE_INDEX);
-    bring_up_color(RED_INDEX);
-
-    // third pattern
-    bring_down_color(BLUE_INDEX);
-    bring_down_color(RED_INDEX);
-    bring_up_color(BLUE_INDEX);
-    bring_down_color(GREEN_INDEX);
-    bring_up_color(RED_INDEX);
-    bring_up_color(GREEN_INDEX);
+    pattern_functions[current_pattern_fun_index]();
 
 }
 
